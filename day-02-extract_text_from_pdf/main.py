a8f2a8178d552944ec78e7737d297ca3ee70862f
baldanaresh <nareshbalda0@gmail.com>
2026-04-26 09:30:40 +0530

    fixed errors and working fine now

--- day-02-extract_text_from_pdf/main.py
+++ day-02-extract_text_from_pdf/main.py
@@ -1,10 +1,11 @@
 from pypdf import PdfReader
+from pypdf.errors import PdfReadError
 
 def extract_text_from_pdf(file_path):
     reader = PdfReader(file_path)
     text=""
-    for page_num,page in enumerate(PdfReader.pages):
-        page_text=page.extract()
+    for page_num,page in enumerate(reader.pages):
+        page_text=page.extract_text()
         if page_text:
             text+=page_text +'\n'
     return text   
@@ -15,3 +16,6 @@ def main():
     text=extract_text_from_pdf(file_path=filepath)
     print("your text is: \n ")
     print(text[:1000])
+
+if __name__ == "__main__":
+    main()
---
+++ day-02-extract_text_from_pdf/mistaked.md
@@ -0,0 +1,4 @@
+instead of applying object .pages to the reader i applied to the pdfreader
+
+if i create a sample.pdf the window is creating a txt file only.
+so always cntrl + p and save as a pdf then only its a pdf 
