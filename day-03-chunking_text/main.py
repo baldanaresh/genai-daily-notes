4eead6673697e52f24e470694454dcbd916f72f1
baldanaresh <nareshbalda0@gmail.com>
2026-04-27 11:00:45 +0530

    chunking correctly

---
+++ day-03-chunking_text/main.py
@@ -0,0 +1,41 @@
+from pypdf import PdfReader
+from pypdf.errors import PdfReadError
+
+def extract_text_from_pdf(file_path):
+    reader = PdfReader(file_path)
+    text=""
+    for page_num,page in enumerate(reader.pages):
+        page_text=page.extract_text()
+        if page_text:
+            text+=page_text +'\n'
+    return text  
+
+def chunk_text(text, chunk_size=500, overlap=100):
+    chunks = []
+    start = 0
+
+    while start < len(text):
+        end = start + chunk_size
+        chunk = text[start:end]
+        chunks.append(chunk)
+
+        start += chunk_size - overlap  # move with overlap
+
+    return chunks
+
+def main():
+    file_path = "sample.pdf"
+    print("hello")
+    text = extract_text_from_pdf(file_path)
+
+    chunks = chunk_text(text)
+
+    print(f"Total chunks: {len(chunks)}\n")
+
+    for i, chunk in enumerate(chunks[:3]):  # preview first 3
+        print(f"--- Chunk {i} ---")
+        print(chunk)
+        print("\n")
+
+if __name__ == "__main__":
+    main()
